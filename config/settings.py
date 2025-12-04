f49a50ce04256ddb3f15e0030b82c1d7f08a1cf5
yohi441 <rikou92991@gmail.com>
2025-12-04 19:24:07 +0800

    restore the mysqlite

--- config/settings.py
+++ config/settings.py
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ai_med_db',
+#         'USER': 'ai_med_user',
+#         'PASSWORD': 'aimedpassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ai_med_db',
-        'USER': 'ai_med_user',
-        'PASSWORD': 'aimedpassword',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
